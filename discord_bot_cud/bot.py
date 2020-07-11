b4c501abd5735dfa5264ab41062afc6b3843a8e5
Andreas Bresser <self@andreasbresser.de>
2020-07-11 17:49:50 +0200

    reshuffle after 5 Minutes

--- discord_bot_cud/bot.py
+++ discord_bot_cud/bot.py
@@ -1,3 +1,4 @@
+import asyncio
 from random import shuffle
 
 import discord
@@ -9,6 +10,11 @@ START_LETTER = '.'
 
 # the default channel name
 MAIN_CHANNEL = 'Allgemein'
+
+# time in seconds when we shuffle again, set to <=0 for manual shuffeling
+SHUFFLE_TIME = 5 * 60
+
+INFO_TIME = 60
 
 
 def round_move(lst: list):
@@ -23,6 +29,9 @@ def round_move(lst: list):
             new[idx+2] = lst[idx]
     return new
 
+
+def format_time(sec):
+    return f'{int(sec/60)}:{sec%60:02d}'
 
 class DiscordCommands:
     """Singleton that stores all known commands in a dict.
@@ -72,6 +81,9 @@ class MyClient(discord.Client):
         self.channels = []
         # current channel we use to get member to move.
         self.main_channel = None
+        # time in seconds we reshuffle
+        self.shuffle_time = SHUFFLE_TIME
+        self.shuffle_timer = None
 
     async def create_channel(self, guild, nr, member):
         channel = await guild.create_voice_channel(
@@ -93,19 +105,80 @@ class MyClient(discord.Client):
         await msg.channel.send(
             f'The channel we use for shuffeling is: "{name}"')
 
+    @DiscordCommands.add('list_channel', 'channel_name')
+    async def list_channel(self, msg, channel_name=None):
+        if channel_name:
+            guild, channel = self.get_voice_channel_by_name(channel_name)
+        elif self.main_channel:
+            channel = self.main_channel
+        else:
+            await msg.channel.send('Sorry, no channel name given')
+            return
+        member = self.get_member_from_channel(channel)
+        txt = f'Members of channel {channel.name} are:\n - '
+        txt += '\n - '.join([m.name for m in member])
+        if not member:
+            txt += '(the channel is empty)'
+        await msg.channel.send(txt)
+
+    @DiscordCommands.add('simulate_shuffle', 'channel_name')
+    async def sim_shuffle_pairs(self, msg, channel_name=MAIN_CHANNEL):
+        if not self.main_channel or self.main_channel.name != channel_name:
+            guild, channel = self.get_channel_by_name(channel_name)
+            self.current_guild = guild
+            self.main_channel = channel
+            self.members = self.main_channel.members
+            shuffle(self.members)
+            await self.make_even(msg)
+
+        # no need to clean up
+
+        # everyone moves one position in list
+        self.members = round_move(self.members)
+
+        # simulate channel creation
+        txt = 'Shuffle user:\n'
+        for idx in range(int(len(self.members)/2)):
+            nr = idx+1
+            member = self.members[idx * 2:idx * 2 + 2]
+            txt += f'group {nr}: {member[0].name} - {member[1].name}\n'
+        await msg.channel.send(txt)
+
+        if self.shuffle_time > 0:
+            self.current_shuffle = self.shuffle_time
+            if not self.shuffle_timer:
+                self.shuffle_timer = self.loop.create_task(
+                    self.reshuffle(msg, self.sim_shuffle_pairs))
+
+    @DiscordCommands.add('shuffle_time', 'time_in_seconds')
+    async def show_edit_shuffle_time(self, msg, time_in_seconds=-2):
+        if int(time_in_seconds) > -2:
+            self.shuffle_time = int(time_in_seconds)
+        t = format_time(self.shuffle_time)
+        await msg.channel.send(f'Shuffle timer is set to: {t}')
+
+    async def make_even(self, msg):
+        if len(self.members) % 2 == 1:
+            txt = 'We are not even! So I have to randomly pick someone...\n' \
+                f'Congratulation, @{self.members[-1].name}! '\
+                'You can freely decide which group to join!'
+            await msg.channel.send(txt)
+            self.members = self.members[:-1]
+
     @DiscordCommands.add('shuffle', 'channel_name')
-    async def shuffle_pairs(self, channel_name=MAIN_CHANNEL):
+    async def shuffle_pairs(self, msg, channel_name=MAIN_CHANNEL):
         if not self.main_channel or self.main_channel.name != channel_name:
             guild, channel = self.get_voice_channel_by_name(channel_name)
             self.current_guild = guild
             self.main_channel = channel
             self.members = self.get_member_from_channel(self.main_channel)
             shuffle(self.members)
+            await self.make_even(msg)
 
         # cleanup - move users and remove old channels if they exist
         self.cleanup()
 
-        # overwrite current member list with shuffled one
+        # everyone moves one position in list
         self.members = round_move(self.members)
 
         for idx in range(int(len(self.members)/2)):
@@ -113,6 +186,23 @@ class MyClient(discord.Client):
                 self.current_guild,
                 idx + 1,
                 self.members[idx * 2:idx * 2 + 2])
+
+        # lets do it again in a few seconds!
+        if self.shuffle_time > 0:
+            self.current_shuffle = self.shuffle_time
+            if not self.shuffle_timer:
+                self.shuffle_timer = self.loop.create_task(
+                    self.reshuffle(msg, self.shuffle_pairs))
+
+    def get_channel_by_name(self, channel_name):
+        for guild in self.guilds:
+            for channel in guild.voice_channels:
+                if channel.name == channel_name:
+                    return guild, channel
+            for channel in guild.text_channels:
+                if channel.name == channel_name:
+                    return guild, channel
+        print(f'Error: could not find channel named {channel_name}')
 
     def get_voice_channel_by_name(self, channel_name):
         for guild in self.guilds:
@@ -127,16 +217,14 @@ class MyClient(discord.Client):
         print(f'Got {len(mem)} users from channel {channel.name}')
         return mem
 
-    @DiscordCommands.add('list_channel', 'channel_name')
-    async def list_channel(self, msg, channel_name):
-        guild, channel = self.get_voice_channel_by_name(channel_name)
-        member = self.get_member_from_channel(channel)
-        txt = f'Members of channel {channel_name} are:\n - '
-        txt += '\n - '.join([m.name for m in member])
-        print(dir(member[0]))
-        if not member:
-            txt += '(the channel is empty)'
-        await msg.channel.send(txt)
+    @DiscordCommands.add
+    async def stop_shuffle(self, msg):
+        if self.shuffle_timer:
+            await msg.channel.send(f'Shuffle stopped!')
+            self.shuffle_timer.cancel()
+            self.shuffle_timer = None
+        else:
+            await msg.channel.send(f'I can not stop - Shuffle is not running!')
 
     @DiscordCommands.add
     async def cleanup(self):
@@ -147,14 +235,16 @@ class MyClient(discord.Client):
             await channel.delete()
         self.channels = []
 
-    async def create_role_and_assign_user(self, role_name="testrolle"):
-        guild = self.main_channel.guild
-        roles = [r for r in guild.roles if r.name == role_name]
-        if len(roles) == 0:
-            roles = [await guild.create_role(name=role_name)]
-        for mem in self.members:
-            for role in roles:
-                await mem.add_roles(role)
+    async def reshuffle(self, msg, shuffle_func):
+        while True:
+            while self.current_shuffle > INFO_TIME:
+                self.current_shuffle -= INFO_TIME
+                await asyncio.sleep(INFO_TIME)
+                time = format_time(self.current_shuffle)
+                await msg.channel.send(f'{time} left!')
+            await asyncio.sleep(self.current_shuffle)
+            self.current_shuffle = 0
+            await shuffle_func(msg, channel_name=self.main_channel.name)
 
     async def on_ready(self):
         """Discord client is ready, set member of channel"""
