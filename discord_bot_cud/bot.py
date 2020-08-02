59646f30cca1b2a68d7c30ae6d6ca0ebaca7822e
Andreas Bresser <self@andreasbresser.de>
2020-08-02 23:52:34 +0200

    translate and minor bugfixes

--- discord_bot_cud/bot.py
+++ discord_bot_cud/bot.py
@@ -1,5 +1,7 @@
 import asyncio
 from random import shuffle
+import pathlib
+import yaml
 
 import discord
 from discord.permissions import Permissions
@@ -16,6 +18,31 @@ SHUFFLE_TIME = 5 * 60
 
 # time in seconds we show how much time is remaining in the main channel
 INFO_TIME = 60
+
+
+default_lang = 'de'
+
+CHANNEL_SUFFIX = '_gruppe'
+
+translations = {}
+
+with open(pathlib.Path().absolute() / 'data' / 'translations.yml') as yaml_file:
+    translations = yaml.full_load(yaml_file)
+
+
+def translate(txt, lang=None):
+    global translations, default_lang
+    if not lang:
+        lang = default_lang
+    if txt in translations[default_lang]:
+        tra = translations[default_lang][txt]
+        if isinstance(tra, list):
+            return rnd.choice(tra)
+        else:
+            return tra
+    else:
+        print(f'not translated: {txt}')
+        return f'TRANSLATE_{txt}'
 
 
 def round_move(lst: list):
@@ -46,6 +73,7 @@ class DiscordCommands:
 
     @staticmethod
     def command_list():
+        """Return command list as text."""
         txt = ''
         for cmd in DiscordCommands.known_commands.keys():
             txt += f'\n - {START_LETTER}{cmd}'
@@ -55,7 +83,7 @@ class DiscordCommands:
         return txt
 
     @staticmethod
-    def add_func(name, func, params=None):
+    def _add_func(name, func, params=None):
         DiscordCommands.known_commands[name] = func
         if params:
             DiscordCommands.command_parameters[name] = params
@@ -64,13 +92,55 @@ class DiscordCommands:
     def add(args=None, *params):
         if isinstance(args, str):
             def decorator(func):
-                DiscordCommands.add_func(args, func, params)
+                DiscordCommands._add_func(args, func, params)
                 return func
             return decorator
         else:
             name = args.__qualname__.rsplit('.', 1)[-1]
-            DiscordCommands.add_func(name, args, params)
+            DiscordCommands._add_func(name, args, params)
             return args
+
+
+def member_name(member):
+    """Small helper to show user name and nick."""
+    if member.nick:
+        return f'{member.name} ({member.nick})'
+    else:
+        return member.name
+
+async def try_to_move_channel(member, channel):
+    try:
+        await member.move_to(channel)
+    except discord.errors.HTTPException:
+        print(translate('cannot_move_user').format(member_name(member), channel.name))
+        await member.send(translate('can_not_move_you').format(channel.name))
+
+def get_channel_by_name(guilds, channel_name):
+    for guild in guilds:
+        for channel in guild.voice_channels:
+            if channel.name == channel_name:
+                return guild, channel
+        for channel in guild.text_channels:
+            if channel.name == channel_name:
+                return guild, channel
+    print(translate('channel_not_found').format(channel_name))
+    return None, None
+
+
+def get_voice_channel_by_name(guilds, channel_name):
+    for guild in guilds:
+        for channel in guild.voice_channels:
+            if channel.name == channel_name:
+                return guild, channel
+    print(translate('channel_not_found').format(channel_name))
+    return None, None
+
+
+def get_member_from_channel(channel):
+    """get all user from the channel"""
+    mem = [m for m in channel.members if not m.bot and m.voice]
+    print(translate('mem_in_chan').format(len(mem), channel.name))
+    return mem
 
 
 class MyClient(discord.Client):
@@ -86,45 +156,61 @@ class MyClient(discord.Client):
         self.shuffle_timer = None
 
     async def create_channel(self, guild, nr, member):
-        channel = await guild.create_voice_channel(
-            f'#{nr}_gruppe')
-        await member[0].move_to(channel)
-        await member[1].move_to(channel)
-        self.channels.append(channel)
-        print(f'group {nr}: {member[0].name} - {member[1].name}')
+        try:
+            name = f'#{nr}{CHANNEL_SUFFIX}'
+            channel = await guild.create_voice_channel(name)
+            await try_to_move_channel(member[0], channel)
+            await try_to_move_channel(member[1], channel)
+            self.channels.append(channel)
+            print(f'{name}: {member[0].name} - {member[1].name}')
+        except Exception:
+            pass
 
     @DiscordCommands.add
     async def list_commands(self, msg):
         await msg.channel.send(
-            'The known commands for this bot are:' +
+            translate('known_commands') +
             DiscordCommands.command_list())
 
     @DiscordCommands.add
     async def channel_name(self, msg):
         name = self.main_channel.name if self.main_channel else MAIN_CHANNEL
         await msg.channel.send(
-            f'The channel we use for shuffeling is: "{name}"')
+            translate('shuffle_channel').format(name))
+
+    @DiscordCommands.add('language', 'language')
+    async def switch_language(self, msg, language=None):
+        global default_lang
+        if language in translations and language != default_lang:
+            await msg.channel.send(translate('switch_from'))
+            default_lang = language
+            await msg.channel.send(translate('switch_to'))
 
     @DiscordCommands.add('list_channel', 'channel_name')
     async def list_channel(self, msg, channel_name=None):
         if channel_name:
-            guild, channel = self.get_voice_channel_by_name(channel_name)
+            guild, channel = get_voice_channel_by_name(
+                self.guilds, channel_name)
         elif self.main_channel:
             channel = self.main_channel
         else:
-            await msg.channel.send('Sorry, no channel name given')
+            await msg.channel.send(translate('no_channel_name'))
             return
-        member = self.get_member_from_channel(channel)
-        txt = f'Members of channel {channel.name} are:\n - '
+        if not channel:
+            await msg.channel.send(
+                translate('voice_channel_not_found').format(channel_name))
+            return
+        member = get_member_from_channel(channel)
+        txt = translate('members').format(channel.name) + '\n - '
         txt += '\n - '.join([m.name for m in member])
         if not member:
-            txt += '(the channel is empty)'
+            txt += translate('empty')
         await msg.channel.send(txt)
 
     @DiscordCommands.add('simulate_shuffle', 'channel_name')
     async def sim_shuffle_pairs(self, msg, channel_name=MAIN_CHANNEL):
         if not self.main_channel or self.main_channel.name != channel_name:
-            guild, channel = self.get_channel_by_name(channel_name)
+            guild, channel = get_channel_by_name(self.guilds, channel_name)
             self.current_guild = guild
             self.main_channel = channel
             self.members = self.main_channel.members
@@ -137,11 +223,12 @@ class MyClient(discord.Client):
         self.members = round_move(self.members)
 
         # simulate channel creation
-        txt = 'Shuffle user:\n'
+        txt = translate('shuffle_user')+':\n'
         for idx in range(int(len(self.members)/2)):
             nr = idx+1
             member = self.members[idx * 2:idx * 2 + 2]
-            txt += f'group {nr}: {member[0].name} - {member[1].name}\n'
+            name = f'#{nr}{CHANNEL_SUFFIX}'
+            txt += f'{name}: {member[0].name} - {member[1].name}\n'
         await msg.channel.send(txt)
 
         if self.shuffle_time > 0:
@@ -152,26 +239,36 @@ class MyClient(discord.Client):
 
     @DiscordCommands.add('shuffle_time', 'time_in_seconds')
     async def show_edit_shuffle_time(self, msg, time_in_seconds=-2):
-        if int(time_in_seconds) > -2:
-            self.shuffle_time = int(time_in_seconds)
-        t = format_time(self.shuffle_time)
-        await msg.channel.send(f'Shuffle timer is set to: {t}')
+        try:
+            if isinstance(time_in_seconds, str):
+                if ':' in time_in_seconds:
+                    if len(time_in_seconds.split(':')) > 2:
+                        return
+                    minutes, seconds = [
+                        int(i) for i in time_in_seconds.split(':')]
+                    time_in_seconds = minutes * 60 + seconds
+            if int(time_in_seconds) > -2:
+                self.shuffle_time = int(time_in_seconds)
+            t = format_time(self.shuffle_time)
+            await msg.channel.send(translate('shuffle_time').format(t))
+        except ValueError:
+            await msg.channel.send(
+                translate('not_time').format(time_in_seconds))
 
     async def make_even(self, msg):
         if len(self.members) % 2 == 1:
-            txt = 'We are not even! So I have to randomly pick someone...\n' \
-                f'Congratulation, @{self.members[-1].name}! '\
-                'You can freely decide which group to join!'
+            txt = translate('not_even').format(self.members[-1].name)
             await msg.channel.send(txt)
             self.members = self.members[:-1]
 
     @DiscordCommands.add('shuffle', 'channel_name')
     async def shuffle_pairs(self, msg, channel_name=MAIN_CHANNEL):
         if not self.main_channel or self.main_channel.name != channel_name:
-            guild, channel = self.get_voice_channel_by_name(channel_name)
+            guild, channel = get_voice_channel_by_name(
+                self.guilds, channel_name)
             self.current_guild = guild
             self.main_channel = channel
-            self.members = self.get_member_from_channel(self.main_channel)
+            self.members = get_member_from_channel(self.main_channel)
             shuffle(self.members)
             await self.make_even(msg)
 
@@ -194,44 +291,21 @@ class MyClient(discord.Client):
                 self.shuffle_timer = self.loop.create_task(
                     self.reshuffle(msg, self.shuffle_pairs))
 
-    def get_channel_by_name(self, channel_name):
-        for guild in self.guilds:
-            for channel in guild.voice_channels:
-                if channel.name == channel_name:
-                    return guild, channel
-            for channel in guild.text_channels:
-                if channel.name == channel_name:
-                    return guild, channel
-        print(f'Error: could not find channel named {channel_name}')
-
-    def get_voice_channel_by_name(self, channel_name):
-        for guild in self.guilds:
-            for channel in guild.voice_channels:
-                if channel.name == channel_name:
-                    return guild, channel
-        print(f'Error: could not find channel named {channel_name}')
-
-    def get_member_from_channel(self, channel):
-        """get all user from the channel"""
-        mem = [m for m in channel.members if not m.bot and m.voice]
-        print(f'Got {len(mem)} users from channel {channel.name}')
-        return mem
-
     @DiscordCommands.add
     async def stop_shuffle(self, msg):
         if self.shuffle_timer:
-            await msg.channel.send('Shuffle stopped!')
+            await msg.channel.send(translate('shuffle_stopped'))
             self.shuffle_timer.cancel()
             self.shuffle_timer = None
         else:
-            await msg.channel.send('I can not stop - Shuffle is not running!')
+            await msg.channel.send(translate('no_shuffle'))
 
     @DiscordCommands.add
     async def cleanup(self):
         """Delete all known channels."""
         for channel in self.channels:
             for member in channel.members:
-                member.move_to(self.main_channel)
+                await try_to_move_channel(member, self.main_channel)
             await channel.delete()
         self.channels = []
 
@@ -248,12 +322,13 @@ class MyClient(discord.Client):
 
     async def on_ready(self):
         """Discord client is ready, set member of channel"""
-        print(f'{self.user} has connected to Discord!')
+        print(translate('ready').format(self.user.name))
 
     async def author_check(self, msg):
         guild_perm = msg.author.guild_permissions
         if guild_perm != Permissions.all():
-            await msg.channel.send(f'Sorry, {msg.author}, you are not Admin!')
+            name = member_name(msg.author)
+            await msg.channel.send(translate('not_admin').format(msg.author))
             return False
         return True
 
@@ -268,7 +343,8 @@ class MyClient(discord.Client):
         commands = DiscordCommands.known_commands
         if txt_command in commands:
             if await self.author_check(msg):
-                await commands[txt_command](self, msg, *txt_params)
+                func = commands[txt_command] 
+                await func(self, msg, *txt_params)
 
 
 def main(token):
